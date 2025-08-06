3d683b0026005dcf3d70142b01bdc30d51cafa44
lukasKyncl <lukas.kyncl21@gmail.com>
2025-08-06 18:51:51 +0200

    přidána komponenta MainPicture

--- src/components/HomeLayout.tsx
+++ src/components/HomeLayout.tsx
@@ -1,13 +1,16 @@
 
 import React from "react";
+
 import Heading from "./Heading";
 import MainParagraph from "./MainParagraph";
+import MainPicture from "./MainPicture";
 
 const HomeLayout: React.FC = () => {
     return (
         <div>
             <Heading />
             <MainParagraph />
+            <MainPicture />
         </div>
     );
 };
---
+++ src/components/MainPicture.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const MainPicture: React.FC = () => {
+    return (
+        <div className="text-center">
+            <img src="images/ff_mk2.jpg" alt="Ford Focus MK2" className="img-fluid" />
+        </div>
+    );
+};
+
+export default MainPicture;
